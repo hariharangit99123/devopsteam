3bc601d90a03fb7b5a13c140416aa906416bd863
hariharangit99123 <118338045+hariharangit99123@users.noreply.github.com>
2022-11-16 13:24:57 +0530

    Add files via upload

---
+++ Areaofcircle.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+public class Areaofcircle
+{
+public static void main(String args[])
+{
+Scanner sc=new Scanner(System.in);
+float pie=3.14f,areaR;
+System.out.println("enter radius");
+int r=sc.nextInt();
+areaR=pie*r*r;
+System.out.println(areaR);
+}
+}
+
